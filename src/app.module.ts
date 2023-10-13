d9adeb2b8a5452156ca0fb00c425aa38eb66ae7d
mavvkel <mic.kusmierz@gmail.com>
2023-10-14 01:23:58 +0200

    Add comments for studying

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,7 +4,12 @@ import { AppService } from './app.service';
 
 @Module({
   imports: [],
+  // LEARN: Remember to put all the written controllers here.
   controllers: [AppController],
+  // LEARN: Services are single-purposed classes that are injected into their
+  // consumers to accomplish some particular goal (f.e. retrieve data).
+  // LEARN: Nest will take care of injecting all the providers (f.e. Services),
+  // into their consumers (f.e. Controllers).
   providers: [AppService],
 })
 export class AppModule {}
